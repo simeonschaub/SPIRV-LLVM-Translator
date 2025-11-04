d7a4715c5d0600c54d5e52f401f9f2880b48ce1f
Simeon David Schaub <simeon@schaub.rocks>
2025-11-04 13:40:19 +0100

    Revert "Update for StringSwitch::Cases deprecation; NFC (#3407)"
    This reverts commit 35827fe27aeaa49c41d7bdd8b33b6cb123b0b422.

--- lib/SPIRV/OCLToSPIRV.cpp
+++ lib/SPIRV/OCLToSPIRV.cpp
@@ -152,8 +152,8 @@ getAtomicPointerMemorySemanticsMemoryMask(const Value *Ptr,
 
 static size_t getOCLCpp11AtomicMaxNumOps(StringRef Name) {
   return StringSwitch<size_t>(Name)
-      .Cases({"load", "flag_test_and_set", "flag_clear"}, 3)
-      .Cases({"store", "exchange"}, 4)
+      .Cases("load", "flag_test_and_set", "flag_clear", 3)
+      .Cases("store", "exchange", 4)
       .StartsWith("compare_exchange", 6)
       .StartsWith("fetch", 4)
       .Default(0);
